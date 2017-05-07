904f099bc37b778f45ccc18188b94a51ad0a0eb5
A.j Nicolas <ajnicolas3517@icloud.com>
2017-05-07 11:15:12 -0500

    Now supports multiple links!
    Don't forget to add a comma after every keyword, or site u add

--- findproducts.py
+++ findproducts.py
@@ -5,56 +5,49 @@
 import requests,crayons
 from bs4 import BeautifulSoup
 
-#Change url site to whatever site you want to scrape
-url = 'https://kith.com/'
+#Change url to whatever site you want to scrape(shopify)
+url = ['https://kith.com','https://shop-usa.palaceskateboards.com']
 
-def linkfriendly():
-    global r
-    global soup
-    
-        #Gets user shopify link
-    headers ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36'
-            '(KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'}
-    fixed = url +'/sitemap_products_1.xml'
-    r = requests.get(fixed ,headers=headers )
-    check = r.status_code
-
-    while True:
-        if check != 200:
-            print crayons.red('Site not found, check link', bold=True)
-        else:
-            print 'Found:',url
-        break
-    soup = BeautifulSoup(r.content, 'html.parser')
-
-linkfriendly()
-
-
-def sites():
-	global links
-	links = []
-	for site in soup.findAll('loc'):
-		links.append(site.text)
-sites()
-
-#input your keywords you want to search for, input as many as you want!
+#input or change keywords you want to search for
 keywords = [
 'yeezy',
 'nmd',
 'boost',
+'sticker',
+'yang',
 ]
 
-def Findamatch():
-	i = 0
-	oglen = len(keywords)
-	while i < oglen:
+headers ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'}
 
-		matching = [s for s in links if keywords[i] in s]
-		print 'FOUND',len(matching), 'LINKS', 'FOR:', keywords[i].upper()
-		for item in matching:
-			print crayons.green(item)
+def findamatch():
+    oglen = len(url)
+    i = 0
+    while i < oglen:
 
-		print '--'*38
-		i += 1
+        fixed = url[i] +'/sitemap_products_1.xml'
+        print ''
+        print crayons.blue(fixed)
+        print crayons.blue('--')*38
 
-Findamatch()
+        r = requests.get(fixed,headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        links = []
+        for site in soup.findAll('loc'):
+            links.append(site.text)
+        i += 1
+
+        t = 0
+        orig = len(keywords)
+        while t < orig:
+
+            matching = [s for s in links if keywords[t] in s]
+            print 'FOUND',len(matching), 'LINKS', 'FOR:', keywords[t].upper()
+            for item in matching:
+                print crayons.green(item)
+
+            print '--'*38
+            t += 1
+
+findamatch()
